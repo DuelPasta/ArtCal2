510855d22603fbb681ff8f37c0434c57397dcc36
Jeronimo PATAT <lambrechts.jasper@gmail.com>
2023-11-19 18:39:51 +0100

    Add project files.

---
+++ ArtCal2/MainScreen.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ArtCal2
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(412, 388);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(117, 59);
+            this.btnParse.TabIndex = 0;
+            this.btnParse.Text = "PARSE";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 577);
+            this.Controls.Add(this.btnParse);
+            this.Name = "MainScreen";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParse;
+    }
+}
+
---
+++ ArtCal2/MainScreen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtCal2
+{
+    public partial class MainScreen : Form
+    {
+        public string filePath = string.Empty;
+        public string fileContent = string.Empty;
+
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+
+                    var fileStream = openFileDialog.OpenFile();
+
+                    StreamReader reader = new StreamReader(fileStream);
+                    // * verwijderen. Export van CC maakt automatisch nieuwe lijnen.
+                    // Naar de toekomst toe , * = newline integreren om ook andere software exports te kunnen gebruiken.
+
+                    string line = "";
+                    string line2 = "";
+                    string multiline = "";
+                    string CommandBlock = "";
+                    char lineEnd = '*';
+                    char commandC = '%';
+                    char ch;
+
+
+                    while (!reader.EndOfStream)
+                    {
+                        ch = (char)reader.Read();
+                    }
+
+                }
+            }
+        }
+    }
+}
